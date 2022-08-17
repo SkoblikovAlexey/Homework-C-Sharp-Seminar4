f018ceaafd96a26079181cb11302a57aa77faa5f
Alexey Skoblikov <happycow32@yandex.ru>
2022-08-17 23:45:13 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+
+    int ApowB(int A, int B)
+    {
+        int result = 1;
+        for (int i = 0; i < B; i++)
+        {
+            result *= A;
+        }
+        return result;
+    }
+    Console.WriteLine("Введите число А");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    int B = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Результат: {ApowB(A, B)}");
+
+    Console.WriteLine("Для завершения введите end, для продолжения - любой символ");
+    string a = Console.ReadLine();
+    if (a == "end")
+    {
+        break;
+    }
+}
